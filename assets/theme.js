0ca0688218eab8125611578900d0aca5cedccfff
localseth <97685638+localseth@users.noreply.github.com>
2023-02-28 09:39:59 -0600

    persist theme preference across page loads

--- assets/theme.js
+++ assets/theme.js
@@ -1,11 +1,46 @@
+const prefersLightTheme = window.matchMedia('(prefers-color-scheme: light)').matches;
+const currentTheme = () => sessionStorage.getItem('theme');
 const root = document.querySelector(':root');
+const themeSwitch = document.querySelector('.switch input');
 // const themeSwitcher = document.getElementById('theme-switcher');
 
+const defaultTheme = () => {
+    if (!prefersLightTheme) {
+        return 'dark';
+    } else {
+        return 'light';
+    }
+};
+
+const initializeTheme = () => {
+    if (currentTheme() === 'dark') {
+        root.classList.add('dark');
+        themeSwitch.checked = true;
+    } else if (root.classList.contains('dark')) {
+        root.classList.remove('dark');
+    }
+}
+
+const checkOrSetTheme = () => {
+    if (sessionStorage.getItem('theme')) {
+        initializeTheme();
+    } else {
+        sessionStorage.setItem('theme', defaultTheme());
+        initializeTheme();
+    }
+}
+
+window.addEventListener('load', checkOrSetTheme);
+
 const themeSwitcher = () => {
-    console.log('theme swticher called');
-    if (root.classList.contains('dark')) {
+    console.log('theme swticher called', currentTheme());
+    if (currentTheme() === 'dark') {
+        sessionStorage.removeItem('theme');
+        sessionStorage.setItem('theme', 'light');
         root.classList.remove('dark');
     } else {
+        sessionStorage.removeItem('theme');
+        sessionStorage.setItem('theme', 'dark');
         root.classList.add('dark');
     }
 };
